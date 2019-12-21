3bff7d20eb447c66609d93a7e600aa63e73e5bed
Leandro Miguel Narciso <lmnnarciso14@gmail.com>
2019-12-21 12:35:35 +0800

    build chapter done

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,12 @@
-import { configure } from '@storybook/react';
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
+import { GlobalStyle } from "../src/shared/global";
+addDecorator(story => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
 
-configure(require.context('../src', true, /\.stories\.js$/), module);
+// automatically import all files ending in *.stories.js
+configure(require.context("../src", true, /\.stories\.js$/), module);
